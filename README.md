11cac9740f051db69e56e24b2b15e6471b9f6c5f
tolstovr <robtolstov@gmail.com>
2024-02-22 21:04:25 +0400

    Template

---
+++ README.md
@@ -0,0 +1,1 @@
+# hackaton_clubs_022024
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+      Hello!<br />
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
+
+export default App;
