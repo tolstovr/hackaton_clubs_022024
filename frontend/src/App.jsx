4eebfe47e44541a6704b2265a3c1a41390756e39
tolstovr <robertproducts2021@gmail.com>
2024-02-24 20:04:25 +0400

    Нихуя не сделал ес честно

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,58 @@
-import './App.css';
-import Button from '@mui/material/Button';
+import React, { useEffect, useState } from 'react'
+import { Button } from '@mui/material'
+import './App.css'
+import Navbar from "./components/Navbar/Navbar"
+import HomePage from "./pages/HomePage/HomePage"
+import ProfilePage from './pages/ProfilePage/ProfilePage'
+import CoursesPage from './pages/CoursesPage/CoursesPage'
+import MyCoursesPage from './pages/MyCoursesPage/MyCoursesPage'
+import LeaderboardPage from './pages/LeaderboardPage/LeaderboardPage'
+import { HomeRounded, SchoolRounded, LeaderboardRounded } from '@mui/icons-material'
 
-function App() {
+const App = () => {
+  const [tab, setTab] = useState("Курсы")
+  const [user, setUser] = useState({
+    id: 1,
+    role: 1,
+    username: "gryazniy_abobus228",
+    password: "jopaspisey228"
+  })
+
+  /*useEffect(() => {
+    const handleUserChanged = () => {
+      setUser(fetch(null))
+    }
+  })*/
+
+  const testUser = {
+    id: 1,
+    role: 1,
+    username: "gryazniy_abobus228",
+    password: "jopaspisey228"
+  }
+
+  const handleAuth = () => {
+    alert("Never gonna give you up")
+  }
+
   return (
     <>
-      Hello!<br />
-      <Button variant="contained">Hello world</Button>
+      <Navbar current={tab} 
+              onClick={setTab} 
+              tabs={[["Главная", <HomeRounded />], ["Курсы", <SchoolRounded />], ["Лидерборды", <LeaderboardRounded />]]}
+              sessionUser={user}
+              handleAuth={handleAuth}/>
+      <pre>{tab}</pre>
+      <main>
+      { tab === "Главная" && <HomePage /> }
+      { tab === "Курсы" && <CoursesPage /> }
+      { tab === "Лидерборды" && <LeaderboardPage /> }
+      { tab === "Мой профиль" && <ProfilePage /> }
+      { tab === "Мои курсы" && <MyCoursesPage /> }
+      </main>
+      
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,6 +6,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
     <App />
   </React.StrictMode>
-);
+)
